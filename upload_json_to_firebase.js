254b4383492b5fa9020efc8733930058b139dedd
Bibhus Luitel <69481703+maharajabhai@users.noreply.github.com>
2021-11-07 07:01:47 -0500

    Create upload_json_to_firebase.js

---
+++ upload_json_to_firebase.js
@@ -0,0 +1,41 @@
+class UploadJsonToFirestore {
+    constructor() {
+        this.firebase_admin_module = require('../firebase_admin')
+        this.FirebaseAdmin = new this.firebase_admin_module()
+        this.fs = require('fs')
+
+    }
+    fromFile(paths, file) {
+        console.log('Uploading')
+        let content = this.fs.readFileSync(file, "utf8")
+        let json = JSON.parse(content)
+        console.log(json)
+        let fireAdmin = this.FirebaseAdmin
+            .firestoreDB()
+
+
+        for (let i = 0; i < paths.length; i++) {
+            if (i == 0) {
+                fireAdmin = fireAdmin.collection(paths[i])
+            } else {
+                let parsePath = paths[i].split('.')
+                if (parsePath.length > 0
+                    && parsePath[parsePath.length - 1] === 'col') {
+                    fireAdmin = fireAdmin.collection(parsePath[0])
+                } else {
+                    fireAdmin = fireAdmin.doc(paths[i])
+                }
+            }
+        }
+        //this.FirebaseAdmin.firestoreDB().collection().doc().set(json)
+
+        fireAdmin.doc('Bzq4c1LpPbkvQQr4oTjm').set(json)
+            .then(() => {
+                console.log('Uploaded')
+            }).catch(error => {
+                console.log(error)
+            })
+    }
+}
+
+module.exports = UploadJsonToFirestore
